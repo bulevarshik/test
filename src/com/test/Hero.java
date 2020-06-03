2e25a218fcf309564d322be3fa87e04c750cf56a
bulevarshik <bulevarshik@gmail.com>
2020-06-03 17:20:01 +0300

    Master dropped

---
+++ src/com/test/Hero.java
@@ -0,0 +1,9 @@
+package com.test;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Hero implements Serializable {
+    Collection col = new ArrayList();
+}
--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -1,9 +1,18 @@
 package com.test;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("Hello World again");
-        System.out.println("Master dropped");
+        Collection collection = new HashSet();
+        collection.add("3");
+        collection.add("2");
+        collection.add("1");
+        for(Object o:collection){
+            System.out.println(o);
+        }
     }
 }
